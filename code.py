cd5b03c43c6f79846a751269168f3f284cd262b7
rajeev-iitdhn <78016390+rajeev-iitdhn@users.noreply.github.com>
2021-01-26 16:06:56 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,4 +2,4 @@ import pandas as pd
 import os
 
 data=pd.read_csv("/home/dell/Desktop/data science/projects/personal_projects/tweet/data/train_2kmZucJ.csv")
-print(data.head())
+print(data.head(10))
